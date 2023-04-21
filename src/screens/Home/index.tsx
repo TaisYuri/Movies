e903f9b98ec3b7cce3937f2ec359e5cb52be1422
TaisYuri <taisyuri@hotmail.com>
2023-04-21 13:08:00 -0300

    lineargradient on Profile screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,6 +8,7 @@ import { Loading } from "../../components/Loading";
 import { Scroll } from "./styles";
 import { IMovies } from "./types";
 import { ListCards } from "../../components/ListCards";
+import { LinearGradient } from "expo-linear-gradient";
 
 export function Home() {
 
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,12 +1,19 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
-
 export function Profile() {
-
   return (
-    <View>
-     <Text>Profile</Text>
+    <View style={{ flex: 1, display: "flex" }}>
+      <LinearGradient
+        start={{ x: 0.0, y: 0.25 }}
+        end={{ x: 0.5, y: 1.0 }}
+        locations={[0, 0.2, 1]}
+        colors={["#5ca896", "#1E1E1E", "#995163"]}
+        // style={{ flex: 1 }}
+      >
+        <Text>Profile</Text>
+      </LinearGradient>
     </View>
   );
 }
