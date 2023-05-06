aa57b3ec714eca6fb68f3096be0b65dc8175ea9f
TaisYuri <taisyuri@hotmail.com>
2023-05-06 19:03:15 -0300

    function dateFormatter

---
+++ src/functions/dateConvert/dateConvert.ts
@@ -0,0 +1,9 @@
+import { format } from "date-fns";
+
+export function dateConvert(date: string){
+    const ISOdateString = date
+    const formateDate = format(new Date(ISOdateString), "dd-MM-yyyy")
+    const formatOnlyYear = format(new Date(ISOdateString), "yyyy")
+    return {formateDate, formatOnlyYear}
+}
+
